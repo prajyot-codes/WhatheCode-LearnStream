3051d7faf824c256f4dc2c35a393b4817e6f330a
abdul razzaq <munshiabdulrazzaq@gmail.com>
2024-12-15 14:40:18 +0530

    Revert "added new files"
    This reverts commit f20813d505b4ae436611f956bbff4c262b2e57d9.

--- backend/src/routes/studentsauth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router =express.Router();
-const {loginStudent,signupStudent}= require('../controllers/userstudentController');
-
-
-router.post('/signup',signupStudent)
-router.post('/login',loginStudent)
-
-export default router
--- backend/src/utils/ApiRespons.js
+++
@@ -1,8 +0,0 @@
-class ApiRespons{
-    constructor(statusCode,data,message = "success"){
-        this.statusCode=statusCode
-        this.data=data
-        this.message=message
-        this.success = statusCode<400
-    }
-}

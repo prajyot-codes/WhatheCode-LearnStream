1ab6effc5e9ee8f3746f6e8c11cf44507d69a0b9
Armunshi <munshiabdulrazzaq@gmail.com>
2025-01-29 00:24:08 +0530

    fix: update axios import path in ViewtheModules component

--- LearnStream/frontend/src/Pages/ViewtheModules.jsx
+++ LearnStream/frontend/src/Pages/ViewtheModules.jsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from '../api/axios';
 import React, { useCallback, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -9,13 +9,12 @@ const ViewtheModules = () => {
     // Memoize the loadModules function
     const loadModules = useCallback(async () => {
         console.log(course_id);
-        
         try {
             const response = await axios.get(`/courses/${course_id}/modules`, {
                 headers: { 'Content-Type': 'application/json' },
                 withCredentials: true,
             });
-            console.log("response: ", response); // Log the fetched data
+            console.log(response); // Log the fetched data
         } catch (error) {
             console.error('Error fetching modules:', error);
         }

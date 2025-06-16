6efe896a701487903c7426ad592d906c54357e83
Abdul Razzaq <munshiabdulrazzaq@gmail.com>
2025-06-16 14:12:22 +0530

    fix: update Vite configuration to set preview port and host

--- LearnStream/frontend/vite.config.js
+++ LearnStream/frontend/vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: 2000, // Change the port number here
-  },
-    plugins: [react()],
-    
-})
+  plugins: [react()],
+  preview: {
+    port: 4173,
+    host: '0.0.0.0'
+  }
+});
